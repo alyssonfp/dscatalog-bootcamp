dd22c07aa9f8091a9897d7cf3dc6b1dd860529cb
alyssonfp <alyssonfp@gmail.com>
2021-07-01 16:45:06 -0300

    Refactor pageable

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -1,9 +1,9 @@
 package com.devsuperior.dscatalog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class DscatalogApplicationTests {
 
 	@Test
