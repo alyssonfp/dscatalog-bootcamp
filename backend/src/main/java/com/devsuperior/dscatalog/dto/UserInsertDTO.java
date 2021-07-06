e60e61059fd44e1d89c744aedda2babd19f9db4e
alyssonfp <alyssonfp@gmail.com>
2021-07-06 17:26:56 -0300

    User Update Validator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -4,7 +4,7 @@ import com.devsuperior.dscatalog.services.validation.UserInsertValid;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO{
-	
+	private static final long serialVersionUID = 1L;
 	
 	private String password;
 	
