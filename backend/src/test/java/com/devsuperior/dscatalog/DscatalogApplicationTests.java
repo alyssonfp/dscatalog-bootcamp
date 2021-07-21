072a4aa035d32b8fdebf7e0d218560aa262a6312
alyssonfp <alyssonfp@gmail.com>
2021-07-21 06:56:01 -0300

    Frontweb crated

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -3,7 +3,6 @@ package com.devsuperior.dscatalog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class DscatalogApplicationTests {
 
