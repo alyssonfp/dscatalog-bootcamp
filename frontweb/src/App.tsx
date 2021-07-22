84eb639f9fd240e6261ea01c040614273cd69f99
alyssonfp <alyssonfp@gmail.com>
2021-07-22 09:29:57 -0300

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,14 @@
+import Home from 'pages/Home'
+
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
 
-const App = () => {
+
+function App()  {
   return (
-    <>
-      <Navbar />
-      <h1> Hello DSCatalog</h1>;
-    </>
+    
+      <Home />
+       
   );
 }
 
