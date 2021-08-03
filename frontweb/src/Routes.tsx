62da95aa68a267b8cdb2b51d7f8b3973cba1c5e8
alyssonfp <alyssonfp@gmail.com>
2021-08-03 14:24:42 -0300

    Auth routes

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -1,34 +1,33 @@
-import Navbar from "components/Navbar";
-import Admin from "pages/Admin";
-import Home from "pages/Home";
-import Catalog from "pages/Catalog";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import ProductDetails from "pages/ProductDetails";
+import Navbar from 'components/Navbar';
+import Admin from 'pages/Admin';
+import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import ProductDetails from 'pages/ProductDetails';
+import Auth from 'pages/Admin/Auth';
 
 const Routes = () => (
-    
-        <BrowserRouter>
-       <Navbar />
-        <Switch>
-            <Route path="/" exact>
-                <Home />
-            </Route>
-            <Route path="/products" exact>
-                <Catalog />
-            </Route>
-            <Route path="/products/:productId">
-                <ProductDetails />
-            </Route>
-            <Redirect from="/admin" to="/admin/products" exact />
-            <Route path="/admin">
-                <Admin />
-            </Route>
-            
-
-        </Switch>
-        
-        </BrowserRouter>
-
-    );
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/products" exact>
+        <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
+      </Route>
+      <Route path="/admin/auth">
+        <Auth />
+      </Route>
+      <Redirect from="/admin" to="/admin/products" exact />
+      <Route path="/admin">
+        <Admin />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
