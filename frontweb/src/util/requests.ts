619cf6744696347a8a9f1c02aeab38e81825be1d
alyssonfp <alyssonfp@gmail.com>
2021-08-05 14:09:36 -0300

    Redirecting to login when 401 or 403

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
+import history from './history';
 
 type LoginResponse = {
   access_token: string;
@@ -8,7 +9,7 @@ type LoginResponse = {
   scope: string;
   userFirstName: string;
   userId: number;
-}
+};
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
@@ -22,14 +23,14 @@ type LoginData = {
   password: string;
 };
 
-export const requestBackendLogin = (loginData : LoginData) => {
+export const requestBackendLogin = (loginData: LoginData) => {
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
     Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
   };
   const data = qs.stringify({
     ...loginData,
-    grant_type: 'password'
+    grant_type: 'password',
   });
   return axios({
     method: 'POST',
@@ -47,12 +48,36 @@ export const requestBackend = (config: AxiosRequestConfig) => {
       }
     : config.headers;
   return axios({ ...config, baseURL: BASE_URL, headers });
-}
+};
 
-export const saveAuthData = (obj : LoginResponse) => {
+export const saveAuthData = (obj: LoginResponse) => {
   localStorage.setItem('tokenKey', JSON.stringify(obj));
-}
+};
 export const getAuthData = () => {
-  const str = localStorage.getItem(tokenKey) ?? "{}";
+  const str = localStorage.getItem(tokenKey) ?? '{}';
   return JSON.parse(str) as LoginResponse;
-}
+};
+
+// Add a request interceptor
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      history.push('/admin/auth');
+    }
+
+    return Promise.reject(error);
+  }
+);
