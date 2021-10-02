7f9f845ac67d0c07db26f492d9bff14f50b808e9
Alysson Pacheco <alyssonfp@gmail.com>
2021-10-02 22:16:13 +0100

    React select layout

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from 'axios';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
+import Select from 'react-select';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 import './styles.css';
@@ -11,6 +12,12 @@ type UrlParams = {
   };
 
 const Form = () => {
+
+    const options = [
+        {value: 'chocolate', label: 'chocolate'},
+        {value: 'morango', label: 'morango'},
+        {value: 'vanilla', label: 'vanilla'},
+    ]
 
     const { productId } = useParams<UrlParams>();
 
@@ -90,6 +97,16 @@ const Form = () => {
                                     {errors.name?.message}
                                 </div>
                             </div>
+
+                            <div className="margin-bottom-30">
+                               <Select 
+                               options={options}
+                               classNamePrefix="product-crud-select"
+                               isMulti
+                               />
+                            </div>
+
+
 
                             <div className="margin-bottom-30">
                                 <input
