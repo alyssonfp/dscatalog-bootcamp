94512442b0738d539781459265c16bf62eef7dd9
Alysson Pacheco <alyssonfp@gmail.com>
2021-10-02 21:38:02 +0100

    delete products, observer pattern

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -12,20 +12,25 @@ const List = () => {
     const [page, setPage] = useState<SpringPage<Product>>();
 
     useEffect(() => {
-        const config: AxiosRequestConfig = {
-          method: 'GET',
-          url: "/products",
-          params: {
-            page: 0,
-            size: 50,
-          }
-        };
-           
-        requestBackend(config)
-          .then((response) => {
-            setPage(response.data);
-          });
+      getProducts();  
       }, []);
+
+      const getProducts = () => {
+        const config: AxiosRequestConfig = {
+            method: 'GET',
+            url: "/products",
+            params: {
+              page: 0,
+              size: 50,
+            }
+          };
+             
+          requestBackend(config)
+            .then((response) => {
+              setPage(response.data);
+            });
+
+      }
     
 
     return (
@@ -41,7 +46,8 @@ const List = () => {
             <div className="row">
                 {page?.content.map(product => (
                     <div key={product.id} className="col-sm-6 col-md-12">
-                    <ProductCrudCard product={product} />
+                    <ProductCrudCard product={product}
+                    onDelete={() => getProducts()} />
                     </div>
                 ))}              
             
