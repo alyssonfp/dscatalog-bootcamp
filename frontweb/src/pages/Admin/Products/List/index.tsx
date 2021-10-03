fe06b646fff471f2638aca7fb66800074830e9bb
Alysson Pacheco <alyssonfp@gmail.com>
2021-10-03 11:44:56 +0100

    Pagination layout

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig } from "axios";
+import Pagination from "components/Pagination";
 import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -50,8 +51,8 @@ const List = () => {
                     onDelete={() => getProducts()} />
                     </div>
                 ))}              
-            
             </div>
+            <Pagination />
         </div>
 
     )
